e2bc75eeabce91a986cbc4ec9630e7dc163961b7
Alan Whitman <alanwhitman@Alans-MacBook-Air.local>
2018-11-02 09:58:34 -0600

    backend mostly done

---
+++ server/controllers/products_controller.js
@@ -0,0 +1,41 @@
+module.exports = {
+    getProducts(appReq, appRes)   {
+        let db = appReq.app.get('db');
+        db.get_products().then(dbRes => {
+            appRes.status(200).send(dbRes);
+        }).catch(err => console.error(err))
+    },
+    getProduct(appReq, appRes)   {
+        let db = appReq.app.get('db');
+        productID = Number(appReq.params.id);
+        db.get_product(productID).then(dbRes => {
+            appRes.status(200).send(dbRes);
+        }).catch(err => console.error(err))
+
+    },
+    createProduct(appReq, appRes)   {
+        let db = appReq.app.get('db');
+        let { name, price, img } = appReq.body;
+        price = Number(price);
+        db.create_product({name, price, img}).then(dbRes => {
+            appRes.status(200).send(dbRes);
+        }).catch(err => console.error(err));
+    },
+    updateProduct(appReq, appRes)   {
+        let db = appReq.app.get('db');
+        let { name, price, img } = appReq.body;
+        let id = Number(appReq.params.id);
+        price = Number(price);
+        db.update_product({ id, name, price, img }).then(dbRes => {
+            appRes.status(200).send(dbRes);
+        }).catch(err => console.error(err));
+    },
+    deleteProduct(appReq, appRes)   {
+        let db = appReq.app.get('db');
+        let id = Number(appReq.params.id);
+        db.delete_product(id).then(dbRes => {
+            appRes.status(200).send(dbRes);
+        }).catch(err => console.error(err));
+
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+const pc = require('./controllers/products_controller');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+let { CONNECTION_STRING: cs, PORT: port} = process.env;
+
+massive(cs).then(db =>  {
+    app.set('db', db);
+    console.log('db is connected');
+});
+
+
+app.get('/api/products', pc.getProducts);
+
+app.get('/api/products/:id', pc.getProduct);
+
+app.post('/api/products', pc.createProduct);
+
+app.put('/api/products/:id', pc.updateProduct);
+
+app.delete('/api/products/:id', pc.deleteProduct);
+
+
+app.listen(port, () => console.log(port));
