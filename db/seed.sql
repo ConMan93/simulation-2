1868155794d6d16f349886c0d1e9df13177c3b30
Alan Whitman <alanwhitman@Alans-MacBook-Air.local>
2018-11-02 14:11:30 -0600

    added seed and init sql files

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO products
+    (name, price, img)
+VALUES
+    ('Thing', 50000, 'https://vignette.wikia.nocookie.net/deathbattlefanon/images/a/a9/The_Thing.png/revision/latest?cb=20150223191205'),
+    ('Shoes', 99, 'https://images.finishline.com/is/image/FinishLine/410453A_014?$Main_gray$'),
+    ('Pants', 750, 'http://dimg.dillards.com/is/image/DillardsZoom/zoom/silver-jeans-co.-zac-relaxed-fit-straight-leg-denim-jeans/04237697_zi_indigo.jpg')
